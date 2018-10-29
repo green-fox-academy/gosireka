ce80d102cd78daa6b3f7fc2e47250a0b9b23f015
Reka Santha-Gosi <gosireka1@gmail.com>
2018-10-29 11:19:13 +0100

    calculate the coding hours in a semester, and the percentage of this

---
+++ week-02/day-1/coding-hours.ts
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('An average Green Fox attendee codes '+ (17*5*6) + ' hours in a semester.');
+console.log('The percentage of the coding hours in the semester is:  '+ ((30/52*100).toFixed(2)));
