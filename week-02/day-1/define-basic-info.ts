c2b7daf67b32b54dbb66b6c9880bba03199ff795
Reka Santha-Gosi <gosireka1@gmail.com>
2018-10-29 11:59:23 +0100

    store personal info in variables and print these

---
+++ week-02/day-1/define-basic-info.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let myName: string = 'Reka Santha-Gosi';
+let myAge: number = 25;
+let myHeight: number = 1.65;
+let isMarried: boolean = true;
+
+console.log('My name: ' + myName + ', my age: ' + myAge + ', my height ' + myHeight + ' m, and I am married: ' + isMarried);
