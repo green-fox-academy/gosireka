b15f8cc6f0951768b203ee95ec4964b5e489825d
Reka Santha-Gosi <gosireka1@gmail.com>
2018-10-29 14:21:36 +0100

    writes the line 100 times

---
+++ week-02/day-1/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let line : string = "I won't cheat on the exam!";
+
+for (let i: number = 0; i < 100; i++){
+    console.log(line);
+}
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
