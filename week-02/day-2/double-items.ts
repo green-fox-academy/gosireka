a70a39ab78329d6f9b91f8e55873873357e257c6
Reka Santha-Gosi <gosireka1@gmail.com>
2018-10-30 15:30:55 +0100

    doubles the strings to a new array with map

---
+++ week-02/day-2/double-items.ts
@@ -0,0 +1,16 @@
+'use strict'
+
+export{}
+
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let drinks = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+
+let newDrinks = drinks.map(function(value){
+        value += value;
+        return value;
+})
+
+console.log(newDrinks);
