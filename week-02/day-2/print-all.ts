dbc6fba4b080ffaa15a463bd7ac8cf197117ffff
Reka Santha-Gosi <gosireka1@gmail.com>
2018-10-30 14:19:27 +0100

    logs each element of an array to the console

---
+++ week-02/day-2/print-all.ts
@@ -0,0 +1,28 @@
+'use strict'
+
+export{}
+
+// -  Create a variable named `integers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `integers` to the console*
+// *hint: use a loop, console.log(integers) won't cut it
+// -  bonus for using the correct built in array method
+
+
+let integers = [4, 5, 6, 7];
+
+/* first try
+function printIntegers (list){
+    for (let i = 0; i < list.length; i++){
+        console.log(list[i]);
+    }
+}
+
+printIntegers(integers);
+*/
+
+
+integers.forEach(function(value){
+    console.log(value);
+});
+
+
