f331630b23fb42146227c9f8135c84cf0af18a87
Reka Santha-Gosi <gosireka1@gmail.com>
2018-10-30 15:06:48 +0100

    changes the value 8 to 4 of an array

---
+++ week-02/day-2/change-element.ts
@@ -0,0 +1,20 @@
+'use strict'
+
+export{}
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+let numList = [1, 2, 3, 8, 5, 6];
+
+let newList = numList.map(function(value){
+    if(value !== 8){
+        return value;
+    }else{
+        value = 4;
+        return value;
+    }
+})
+
+console.log(newList[3]);
