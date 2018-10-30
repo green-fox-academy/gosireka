0413f5224266e6cd11e601583b79d7ee2d8cb442
Reka Santha-Gosi <gosireka1@gmail.com>
2018-10-30 10:18:59 +0100

    plus 'use strict'

--- week-02/day-2/doubling.ts
+++ week-02/day-2/doubling.ts
@@ -1,3 +1,6 @@
+'use strict'
+
+export{}
 
 // -  Create a variable named `baseNum` and assign the value `123` to it
 // -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
