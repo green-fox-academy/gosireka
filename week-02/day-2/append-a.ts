52c6926bdad81460559fe7f52ddea8f135875122
Reka Santha-Gosi <gosireka1@gmail.com>
2018-10-30 15:21:14 +0100

    adds an a character to each element of the array

---
+++ week-02/day-2/append-a.ts
@@ -0,0 +1,19 @@
+'use strict'
+
+export{}
+
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals = ["koal", "pand", "zebr"];
+
+let char = 'a';
+
+let newAnimals = animals.map(function(value){
+        value += char;
+        return value;
+})
+
+console.log(newAnimals);
+
