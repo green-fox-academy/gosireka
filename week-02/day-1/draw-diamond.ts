bd550bfa4d71f244776743b4efe19f4d9ab81f6c
Reka Santha-Gosi <gosireka1@gmail.com>
2018-10-31 11:07:55 +0100

    draws a diamond shape by symbols

---
+++ week-02/day-1/draw-diamond.ts
@@ -0,0 +1,32 @@
+'use strict'
+
+export{}
+
+let lineCount: number = 7;
+let space: string = ' ';
+let asterisk: string = '*'
+
+for (let row: number= 0; row < lineCount; row++){
+    if( row < lineCount/2){
+        let asteriskCount: number = 1 + row * 2;
+        let spaceCount: number = (lineCount - asteriskCount) / 2 ;
+        console.log(space.repeat(spaceCount) + asterisk.repeat(asteriskCount));
+    }else{
+        let asteriskCount: number = (lineCount - (row + 1)) * 2 + 1;
+        let spaceCount: number = (lineCount - asteriskCount) / 2;
+        console.log(space.repeat(spaceCount) + asterisk.repeat(asteriskCount));
+    }
+}
+
+// Write a program that draws a
+// diamond like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+//
+// The diamond should have as many lines as lineCount is
