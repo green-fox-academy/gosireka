cae01637c66d4dbc75b67a0c2d7d05ee7abc72f8
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-04 20:26:48 +0100

    better solution: draws diagonal in square

--- week-02/day-1/draw-diagonal.ts
+++ week-02/day-1/draw-diagonal.ts
@@ -2,17 +2,17 @@
 
 export{}
 
-let lineCount: number = 12;
-let percentage: string = '%';
+let lineCount: number = 6;
+let symbol: string = '%';
 let space: string = ' ';
 
-for (let i: number = 0; i < lineCount; i++) {
-  if (i === 0 || i === lineCount-1){
-    console.log(percentage.repeat(lineCount-1));
-  }else if (i === lineCount-2){
-    console.log(percentage + space.repeat(i-1) + percentage)
-  }else{
-    console.log(percentage + space.repeat(i-1) + percentage + space.repeat(lineCount-3-i) + percentage);
+for (let actualRow: number = 0; actualRow < lineCount; actualRow++) {
+  if (actualRow === 0 || actualRow === lineCount - 1) {
+    let row: string = symbol.repeat(lineCount);
+    console.log(row);
+  } else {
+    let row: string = symbol + space.repeat(actualRow - 1) + symbol + space.repeat(lineCount - 3 -(actualRow-1)) + symbol;
+    console.log(row);
   }
 }
 
