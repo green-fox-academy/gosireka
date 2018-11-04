3c56c0a48df83a70184a03eafbc9e54d4957d450
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-04 16:51:28 +0100

    better solution and clean up: draws pyramid by stars

--- week-02/day-1/draw-pyramid.ts
+++ week-02/day-1/draw-pyramid.ts
@@ -1,36 +1,26 @@
 'use strict';
+'use strict';
+
+let lineCount: number = 4;
+let star: string = '*';
+let space: string = ' ';
+
+for (let actualRow = 1; actualRow <= lineCount; actualRow++) {
+    let spaceCount: number = lineCount - actualRow;
+    let starCount: number = 1 + ((actualRow - 1) * 2);
+    let row: string = space.repeat(spaceCount) + star.repeat(starCount);
+    console.log(row);
+}
+
+/* My first solution: 
 
 let lineCount: number = 4;
 let asterisk: string = '*';
-let space:  string =' ';
+let space:  string = ' ';
 let addAsterisk: string = '**';
 
-for (let i: number = 1; i <= lineCount; i++){
-    console.log((space.repeat(lineCount-i) + asterisk));
+for (let i: number = 1; i <= lineCount; i++) {
+    console.log(space.repeat(lineCount-i) + asterisk);
     asterisk += addAsterisk;
 }
-
-// First try
-/*
-let space1: string = ' ';
-let space2: string = space1 + space1;
-let space3: string = space2 + space1;
-let asterisk: string = ' ';
-
-for (let i: number = 1; i <= lineCount; i++){
-    if(i==1){
-        asterisk += '*';
-        console.log(space3 + asterisk);
-    }else if (i==2){
-        asterisk += '**';
-        console.log(space2 + asterisk);
-    }else if (i ==3){
-        asterisk += '**';
-        console.log(space1 + asterisk);
-    }else{
-        asterisk += '**';
-        console.log(space1 + asterisk);
-    }
-}
-
 */
