92e98589054ad5428070f0af8ebefef4dcd412a3
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-04 16:26:07 +0100

    clean up + alternative solution for draw a triangle by stars

--- week-02/day-1/draw-triangle.ts
+++ week-02/day-1/draw-triangle.ts
@@ -1,12 +1,23 @@
 'use strict';
 
 let lineCount: number = 4;
+let star: string = '*';
+
+for (let i: number = 1; i <= lineCount; i++) {
+  console.log(star.repeat(i));
+}
+
+/* alternative solution
+
+let lineCount: number = 4;
 let asterisk: string = ' ';
 
-for (let i: number = 1; i <= lineCount; i++){
+for (let i: number = 1; i <= lineCount; i++) {
     asterisk += '*';
     console.log(asterisk);
 }
+
+*/
 
 // Write a program that draws a triangle like this:
 //
