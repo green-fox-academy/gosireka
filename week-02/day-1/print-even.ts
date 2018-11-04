29216fb6a01a92ad74c69cd6ed1f55c39a1bcdf8
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-04 16:05:16 +0100

    clean up and alternative solution: prints even numbers between 0 and 500

--- week-02/day-1/print-even.ts
+++ week-02/day-1/print-even.ts
@@ -1,7 +1,17 @@
 'use strict';
 
-for (let i: number = 0; i <=500; i+=2){
+for (let i: number = 0; i <= 500; i += 2) {
     console.log(i);
 }
 
 // Create a program that prints all the even numbers between 0 and 500
+
+/* Other solution: 
+
+for (let i: number = 0; i <= 500; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+*/
