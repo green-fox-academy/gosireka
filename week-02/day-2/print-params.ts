166d44465befe5ac60e7e379eac6ac88454741ec
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-04 21:41:30 +0100

    clean up: function that prints parameters

--- week-02/day-2/print-params.ts
+++ week-02/day-2/print-params.ts
@@ -6,7 +6,7 @@ export{}
 //    which logs to the console the input parameters
 //    (can have multiple number of arguments)
 
-function printParams (...theArgs){
+function printParams (...theArgs) {
     console.log(arguments);
 }
 
