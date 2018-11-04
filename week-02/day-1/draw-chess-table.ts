b1d23255ec152b38fa29cba972e7eda4dac8aedf
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-04 20:48:11 +0100

    clean up: draws chess table by symbols

--- week-02/day-1/draw-chess-table.ts
+++ week-02/day-1/draw-chess-table.ts
@@ -2,13 +2,16 @@
 
 let lineCount: number = 8;
 let space: string = ' ';
-let percentage: string = '% ';
+let percentage: string = '%';
+let spaceAndPercent: string = percentage.concat(space);
 
-for(let i: number = 1; i <=lineCount; i++){
-    if(i % 2 !== 0){
-        console.log(percentage.repeat(lineCount/2));
-    }else{
-        console.log(space + percentage.repeat(lineCount/2));
+let row: string = spaceAndPercent.repeat(lineCount/2);
+
+for (let i: number = 0; i < lineCount; i++) {
+    if (i % 2 === 0) {
+        console.log(row);
+    } else {
+        console.log(space + row);
     }
 }
 
