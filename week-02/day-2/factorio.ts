d27a4d2201914277eb4c771d9a3aaf5a858c1d1b
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-04 21:35:58 +0100

    clean up: function that returns a given numbers factorial

--- week-02/day-2/factorio.ts
+++ week-02/day-2/factorio.ts
@@ -5,12 +5,11 @@ export{}
 // -  Create a function called `factorio`
 //    that returns it's input's factorial
 
-let givenNumber = 6;
+let givenNumber: number = 4;
 
-
-function factorio(num: number){
+function factorio(num: number): number {
     let fact: number = 1;
-    for(let i:number = 1; i <=givenNumber; i++){
+    for (let i:number = 1; i <= givenNumber; i++) {
         fact *=i;
     }
     return fact;
