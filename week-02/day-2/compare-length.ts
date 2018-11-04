166d9f2969fe0191bb5889a4396eef7dba8ded52
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-04 21:51:39 +0100

    better solution: compares two arrays length

--- week-02/day-2/compare-length.ts
+++ week-02/day-2/compare-length.ts
@@ -11,10 +11,4 @@ export{}
 let firstList = [1, 2, 3];
 let secondList = [4, 5];
 
-if (secondList.length > firstList.length){
-    console.log("the second list has " + (secondList.length - firstList.length) + " more element(s)");
-}else if (secondList.length < firstList.length){
-    console.log("the first list has " + (firstList.length - secondList.length) + " more elements");
-}else{
-    console.log("the two list have equal elements:  " + firstList.length);
-}
+console.log('The second list has more element: ' + (firstList.length < secondList.length));
