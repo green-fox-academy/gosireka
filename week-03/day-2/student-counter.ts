fc182754286b2af4321f388647ae5270c303e7e8
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-07 22:21:45 +0100

    takes a list of students and logs sum

---
+++ week-03/day-2/student-counter.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+const students: any[] = [
+  {name: 'Theodor', age: 3, candies: 2},
+  {name: 'Paul', age: 9.5, candies: 2},
+  {name: 'Mark', age: 12, candies: 5},
+  {name: 'Peter', age: 7, candies: 3},
+  {name: 'Olaf', age: 12, candies: 7},
+  {name: 'George', age: 10, candies: 1}
+];
+
+function sumCandies (studentList) {
+  let sumOfCandies = 0;
+  for (let i = 0; i < studentList.length; i++) {
+    let student = studentList[i];
+    sumOfCandies += student.candies;
+  }
+  return sumOfCandies;
+}
+
+function sumAge (studentList) {
+  let sumOfAge = 0;
+  for (let i = 0; i < studentList.length; i++) {
+    let student = studentList[i];
+    if (student.candies < 5){
+      sumOfAge += student.age;
+    }
+  }
+  return sumOfAge;
+}
+
+console.log(sumCandies(students));
+console.log(sumAge(students));
+
+// create a function that takes a list of students and logs: 
+// - how many candies are owned by students
+
+// create a function that takes a list of students and logs:
+// - Sum of the age of people who have lass than 5 candies
