1cf0d108af55c6070ae8cc6dbf59bae863be86c2
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-09 09:35:01 +0100

    line play from two sides

---
+++ week-03/day-4/lineplay.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+for (let i = 0; i < canvas.width; i += 30){
+  drawLine ([i, 0],[canvas.width, i], 'purple');
+  drawLine ([0, i], [i, canvas.height], 'green');
+}
+
+function drawLine (fromCoord, toCoord, color){
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(fromCoord[0], fromCoord[1]);
+  ctx.lineTo(toCoord[0], toCoord[1]);
+  ctx.stroke();
+}
