220a7e93f81fef0bdc838fdd0b354680074e7bcc
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-10 12:55:58 +0100

    clean up: starry night canvas

--- week-03/day-3/starry-night/starry-night-ts.ts
+++ week-03/day-3/starry-night/starry-night-ts.ts
@@ -8,17 +8,17 @@ const ctx = canvas.getContext('2d');
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-let starNum = 150;
+let starNum: number = 150;
 
-for (let i = 0; i < starNum; i++ ) {
-  let x = Math.floor(Math.random() * canvas.width); 
-  let y = Math.floor(Math.random() * canvas.height); 
-  let random = Math.floor(Math.random() * 256);
-  let color = 'rgb(' + random + ', ' + random + ', ' + random + ')';
+for (let i: number = 0; i < starNum; i++) {
+  let x: number = Math.floor(Math.random() * canvas.width);
+  let y: number = Math.floor(Math.random() * canvas.height);
+  let random: number = Math.floor(Math.random() * 256);
+  let color: string = `rgb( ${random}, ${random}, ${random})`;
   drawStar(x, y, color);
 }
 
-function drawStar(x, y, color) {
+function drawStar(x: number, y: number, color: string) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, 3, 3);
 }
