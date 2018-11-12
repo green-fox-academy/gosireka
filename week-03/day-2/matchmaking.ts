5e765e7bba4418a57113b6691be5e9abf0dd8f3a
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-12 20:36:47 +0100

    clean up: matchmaking.ts

--- week-03/day-2/matchmaking.ts
+++ week-03/day-2/matchmaking.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-export{}
+export { }
 
 // Write a function that joins two array by matching one girl with one boy in a new array
 // Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
@@ -8,9 +8,9 @@ export{}
 let girls: string[] = ['Eve', 'Ashley', 'Claire', 'Kat', 'Jane'];
 let boys: string[] = ['Joe', 'Fred', 'Tom', 'Todd', 'Neef', 'Jeff'];
 
-function makingMatches(firstArr, secondArr) {
-  let mixedArr = [];
-  for (let i = 0; i < firstArr.length; i++) {
+function makingMatches(firstArr: string[], secondArr: string[]): string[] {
+  let mixedArr : string[] = [];
+  for (let i: number = 0; i < firstArr.length; i++) {
     mixedArr.push(firstArr[i]);
     mixedArr.push(secondArr[i]);
   }
