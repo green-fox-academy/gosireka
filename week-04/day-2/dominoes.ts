9006b94cd138491b03a3e265481f16da9719a96c
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-13 21:34:24 +0100

    puts the dominoes in correct order

---
+++ week-04/day-2/dominoes.ts
@@ -0,0 +1,37 @@
+import { Domino } from "./domino";
+
+function initializeDominoes(): Domino[] {
+  let dominoes = [];
+  dominoes.push(new Domino(5, 2));
+  dominoes.push(new Domino(4, 6));
+  dominoes.push(new Domino(1, 5));
+  dominoes.push(new Domino(6, 7));
+  dominoes.push(new Domino(2, 4));
+  dominoes.push(new Domino(7, 1));
+  return dominoes;
+}
+
+let dominoes = initializeDominoes();
+/** You have the list of Dominoes */
+/** Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides */
+/** eg: [2, 4], [4, 3], [3, 5] ... */
+
+function putInOrder(dominoes: Domino[]) {
+  for (let i: number = 1; i < dominoes.length; i++) {
+    while (dominoes[i - 1].values[1] !== dominoes[i].values[0]) {
+      let removedDominos : Domino[] = dominoes.splice(i, 1);
+      dominoes.push(removedDominos[0]);
+    }
+  }
+  return dominoes;
+}
+
+putInOrder(dominoes);
+
+function print(dominoes: Domino[]) {
+  dominoes.forEach(function (value) {
+    console.log(value);
+  });
+}
+
+print(dominoes);
