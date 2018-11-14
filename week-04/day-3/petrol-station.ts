d4a2d1b819a63fd1c68f8656b21dda07c613974f
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-14 11:40:59 +0100

    petrol station and cars

---
+++ week-04/day-3/petrol-station.ts
@@ -0,0 +1,60 @@
+import { stat } from "fs";
+
+/*
+Create a Station and a Car classes
+Station
+  gasAmount
+  refill(car) -> decreases the gasAmount by the capacity of the car and increases the cars gasAmount
+Car
+  gasAmount
+  capacity
+  create constructor for Car where:
+    initialize gasAmount -> 0
+    initialize capacity -> 100
+*/
+
+class Station {
+  gasAmount: number;
+  constructor(gasAmount: number) {
+    this.gasAmount = gasAmount;
+  }
+  refill(car: Car) {
+    this.gasAmount -= car.capacity - car.gasAmount;
+    car.gasAmount += car.capacity - car.gasAmount;
+  }
+}
+
+class Car {
+  gasAmount: number;
+  capacity: number;
+  constructor(gasAmount: number = 0, capacity: number = 100) {
+    this.gasAmount = gasAmount;
+    this.capacity = capacity;
+  }
+}
+
+function initializeCars(): Car[] {
+  let carArr: Car[] = [];
+  carArr.push(new Car(10, 60));
+  carArr.push(new Car(6, 90));
+  carArr.push(new Car());
+  return carArr;
+}
+let carArr = initializeCars();
+let station = new Station(100);
+
+console.log(carArr);
+console.log('-------------');
+
+carArr.forEach(function (actualCar) {
+  if (actualCar.capacity - actualCar.gasAmount <= station.gasAmount) {
+    while (actualCar.gasAmount < actualCar.capacity) {
+      station.refill(actualCar)
+      console.log('remained gas in the station: ' + station.gasAmount);
+    }
+  } else {
+    console.log('there is not enough gas in the station');
+  }
+})
+
+console.log(carArr);
