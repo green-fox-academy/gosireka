3745f5fd788ff5bebc0efcfcaec1e8ed599a29d8
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-17 10:47:59 +0100

    clean-up: power.ts

--- week-04/day-4/power.ts
+++ week-04/day-4/power.ts
@@ -2,13 +2,11 @@
 // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 
 function power(base: number, n: number): number {
-  let result = base;
   if (n === 0) {
     return 1;
   } else {
-    result *= power(base, n - 1);
+    return base * power(base, n - 1);
   }
-  return result;
 }
 
 console.log(power(3, 3));
