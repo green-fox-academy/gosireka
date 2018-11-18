f9e9ef0619ff5ff7f70be551822b8220f656ad8b
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-18 20:27:33 +0100

    reverse lines

---
+++ week-04/day-1/reversed-lines.ts/reversed-lines.ts
@@ -0,0 +1,27 @@
+export { };
+const fs = require('fs');
+
+// Create a method that decrypts reversed-lines.txt
+
+function readFromFile(fileName: string): string {
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  } catch (e) {
+    return null;
+  }
+}
+
+function reverseLines(fileName: string): string {
+  const fileContent = readFromFile(fileName);
+  let lines: string[] = fileContent.split('\n');
+  let newString: string = '';
+  for (let i: number = 0; i < lines.length; i++) {
+    for (let j: number = lines[i].length - 1; j >= 0; j--) {
+      newString = newString.concat(lines[i].charAt(j));
+    }
+    newString = newString.concat('\n');
+  }
+  return newString;
+}
+
+console.log(reverseLines('reversed-lines.txt'));
