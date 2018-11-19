9f8e5a4d52355c7ad7176d88280541160eda5f59
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-19 12:18:12 +0100

    feat(inheritance exercise): add student class

---
+++ week-05/day-1/student.ts
@@ -0,0 +1,21 @@
+import { Person } from './person';
+
+export class Student extends Person {
+  protected previousOrganization: string;
+  protected skippedDays: number;
+
+  constructor(name?: string, age?: number, gender?: string, previousOrganization?: string) {
+    super(name, age, gender);
+    this.skippedDays = 0;
+    this.previousOrganization = previousOrganization === undefined ? 'School of life' : previousOrganization;
+  }
+  getGoal() {
+    console.log('My goal is: Be a junior software developer.');
+  }
+  introduce() {
+    console.log(`Hi, I\'m ${this.name}, a ${this.age} year old ${this.gender} from ${this.previousOrganization} who skipped ${this.skippedDays} days from the course already.`)
+  }
+  skipDays(numberOfDays: number) {
+    this.skippedDays += numberOfDays;
+  }
+}
