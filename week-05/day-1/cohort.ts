a6196bf9f7d6b51905d8b4bc552f746a460102c4
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-19 12:21:18 +0100

    feat(inheritance exercise): add cohort class

---
+++ week-05/day-1/cohort.ts
@@ -0,0 +1,23 @@
+import { Student } from './student';
+import { Mentor } from './mentor';
+
+export class Cohort {
+  protected name: string;
+  protected students: Student[];
+  protected mentors: Mentor[];
+
+  constructor(name: string) {
+    this.name = name;
+    this.students = [];
+    this.mentors = [];
+  }
+  addStudent(student: Student) {
+    this.students.push(student);
+  }
+  addMentor(mentor: Mentor) {
+    this.mentors.push(mentor);
+  }
+  info() {
+    console.log(`The ${this.name} cohort has ${this.students.length} students and ${this.mentors.length} mentors.`)
+  }
+}
