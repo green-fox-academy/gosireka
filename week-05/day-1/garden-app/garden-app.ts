e8f85544da84a18fe218f5223aeec62415b18dca
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-19 19:21:33 +0100

    feat(garden): add garden-app

---
+++ week-05/day-1/garden-app/garden-app.ts
@@ -0,0 +1,43 @@
+import { Flower } from './flower';
+import { Tree } from './tree';
+import {Garden} from './garden';
+
+let myGarden = new Garden();
+
+myGarden.addTree(new Tree('purple'));
+myGarden.addTree(new Tree('orange'));
+myGarden.addFlower(new Flower('yellow'));
+myGarden.addFlower(new Flower('blue'));
+
+console.log(myGarden);
+
+myGarden.getFlowers().forEach(function (actualFlower){
+  actualFlower.info();
+})
+myGarden.getTrees().forEach(function (actualFlower){
+  actualFlower.info();
+})
+
+myGarden.wateringGarden(40);
+
+console.log('--------------');
+
+myGarden.getFlowers().forEach(function (actualFlower){
+  actualFlower.info();
+})
+myGarden.getTrees().forEach(function (actualFlower){
+  actualFlower.info();
+})
+
+myGarden.wateringGarden(70);
+
+console.log('--------------');
+
+myGarden.getFlowers().forEach(function (actualFlower){
+  actualFlower.info();
+})
+myGarden.getTrees().forEach(function (actualFlower){
+  actualFlower.info();
+})
+
+console.log(myGarden);
