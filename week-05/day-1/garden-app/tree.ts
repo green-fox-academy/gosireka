e3c4bd7d55b542a135bab9aba6540025f95f2411
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-19 19:17:13 +0100

    feat(garden): add Tree class

---
+++ week-05/day-1/garden-app/tree.ts
@@ -0,0 +1,25 @@
+import { Plant } from './plant';
+export class Tree extends Plant {
+
+  constructor(color: string) {
+    super(color);
+    this.type = 'Tree';
+  }
+  watering(amount: number): void {
+    if (this.needWater === true && this.waterAmount < 10) {
+      this.waterAmount += amount * 0.4;
+      this.needWater = this.checkNeed();
+    } else {
+      this.needWater === false;
+      console.log('This tree does not need water');
+    }
+
+  }
+  checkNeed(): boolean{
+    if(this.waterAmount < 10){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
