75163816c8f5084f979d581d31dbb7a7df6a6826
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-22 15:54:34 +0100

    feat(fibonacci): add fibonacci function

---
+++ week-05/day-4/fibonacci/fibonacci.ts
@@ -0,0 +1,10 @@
+export function fibonacci(index: number): number {
+  let integerIndex : number = Math.round(index);
+  if (integerIndex <= 0 || index === undefined || index === null) {
+    return 0;
+  } else if (integerIndex === 1) {
+    return 1;
+  } else {
+    return (fibonacci(integerIndex - 1) + fibonacci(integerIndex - 2));
+  }
+}
