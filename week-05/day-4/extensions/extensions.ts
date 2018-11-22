ec4e753eaa52251d25535b6a6c02edc7089a9c8b
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-22 17:20:31 +0100

    fix(extensions): fix the wrong functions

---
+++ week-05/day-4/extensions/extensions.ts
@@ -0,0 +1,50 @@
+'use strict';
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function maxOfThree(a: number, b: number, c: number): number {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else {
+    return c;
+  }
+};
+
+export function median(pool: number[]): number {
+  let orderedPool = pool.sort(function (a, b) {
+    return a - b;
+  });
+  if (pool.length % 2 !== 0) {
+    return orderedPool[Math.floor(pool.length / 2)]
+  } else {
+    return (orderedPool[Math.floor((pool.length) / 2)] + orderedPool[Math.floor((pool.length) / 2) - 1]) / 2;
+  }
+}
+
+export function isVowel(character: string): boolean {
+  let lowerCaseChar = character.toLowerCase();
+  return ['a', 'u', 'o', 'e', 'i'].some(vowel => vowel === lowerCaseChar);
+}
+
+export function translate(hungarian: string): string {
+  let teve = hungarian;
+  let length = teve.length;
+  let checkedVowels: string[] = [];
+
+  for (let i = 0; i < length; i++) {
+    let c = teve[i];
+    if (isVowel(c) && checkedVowels.indexOf(c) === -1) {
+      teve = teve.split(c).join(`${c}v${c}`);
+      i += 2;
+      length += 2;
+      checkedVowels.push(c);
+    }
+  }
+  return teve;
+}
+
+console.log('alma'.split('a').join());
