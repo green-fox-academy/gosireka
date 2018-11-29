99ba325f8ed432095ea8c7b8ad1eedbf33717861
Reka Santha-Gosi <gosireka1@gmail.com>
2018-11-29 16:48:13 +0100

    fix(anagram): change test equals to ok/notOK

--- week-05/day-4/anagram/tests.ts
+++ week-05/day-4/anagram/tests.ts
@@ -2,13 +2,13 @@ import { test } from 'tape';
 import { areAnagrams } from './anagram';
 
 test('check anagram', t => {
-  t.equal(areAnagrams('abb', 'bab'), true);
-  t.equal(areAnagrams('bbb', 'bab'), false);
-  t.equal(areAnagrams('bbb', 'babb'), false);
+  t.ok(areAnagrams('abb', 'bab'));
+  t.notOk(areAnagrams('bbb', 'bab'));
+  t.notOk(areAnagrams('bbb', 'babb'));
   t.end();
 })
 
 test('check anagrams with capital letter', t =>{
-  t.equal(areAnagrams('Test', 'test'), true);
+  t.ok(areAnagrams('Test', 'test'));
   t.end();
 })
