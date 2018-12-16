8ac24122ca18521f10a5d51a547afd4da683612b
Reka Santha-Gosi <gosireka1@gmail.com>
2018-12-16 10:26:46 +0100

    fix(prime-square): remove comments

--- week-08/day-1/prime-squares/main.js
+++ week-08/day-1/prime-squares/main.js
@@ -1,11 +1,4 @@
-
-// 2: Create a function that adds a 'not-prime' class to a div if it's not a prime and 'prime' if it is
-// 3: Create a timer that keeps calling the prime validatior function until it reaches the end of elements
-//   - the timer should fire every 100ms
-//     - the timer should stop when there are no more elements left to be colored
-
 const section = document.querySelector('section');
-
 
 function generateDivs() {
   for (let i = 1; i < 100; i++) {
