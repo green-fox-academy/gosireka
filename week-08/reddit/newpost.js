f01f8c8f9bca98dbeaf0005700684aafb8bbab37
Reka Santha-Gosi <gosireka1@gmail.com>
2018-12-21 09:10:04 +0100

    feat(reddit): change button click to form submit listener

--- week-08/reddit/newpost.js
+++ week-08/reddit/newpost.js
@@ -6,22 +6,24 @@ let title = document.querySelector('textarea');
 let url = document.querySelector('input');
 let form = document.querySelector('form');
 
-submit.addEventListener('click', ()=>{
-  event.preventDefault();
-  const body = {
-    'title': title.value,
-    'url' : url.value,
-    'timestamp': Date.now()
-  }
-  xhr.open('POST', `${base_url}/posts`);
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      window.location.replace('file:///Users/reka/Documents/greenfox/gosireka/week-08/reddit/reddit.html');
+form.addEventListener('submit', () => {
+  if(!title.value || !url.value){
+    alert('Please give a title and url!');
+  } else {
+    event.preventDefault();
+    const body = {
+      'title': title.value,
+      'url' : url.value,
+      'timestamp': Date.now()
     }
+    xhr.open('POST', `${base_url}/posts`);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        window.location.replace('file:///Users/reka/Documents/greenfox/gosireka/week-08/reddit/reddit.html');
+      }
+    }
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(body));
   }
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify(body));
 });
-
-
