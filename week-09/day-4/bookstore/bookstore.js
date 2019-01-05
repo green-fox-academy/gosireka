1c10dbec541c2885a71e986786b1e5ba4294ff8a
Reka Santha-Gosi <gosireka1@gmail.com>
2019-01-05 17:43:19 +0100

    feat(bookstore): add books by price query

--- week-09/day-4/bookstore/bookstore.js
+++ week-09/day-4/bookstore/bookstore.js
@@ -24,41 +24,52 @@ const getAllBooks = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_pr
 const getBooksByCategory = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM ((book_mast INNER JOIN author ON author.aut_id = book_mast.aut_id) INNER JOIN category ON book_mast.cate_id = category.cate_id) INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id WHERE category.cate_descrip = ?;';
 const getBooksByPublisher = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM ((book_mast INNER JOIN author ON author.aut_id = book_mast.aut_id) INNER JOIN category ON book_mast.cate_id = category.cate_id) INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id WHERE publisher.pub_name = ?;';
 const getBooksByCategoryAndPublisher = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM ((book_mast INNER JOIN author ON author.aut_id = book_mast.aut_id) INNER JOIN category ON book_mast.cate_id = category.cate_id) INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id WHERE category.cate_descrip = ? && publisher.pub_name = ?;';
+const getBooksByPrice = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM ((book_mast INNER JOIN author ON author.aut_id = book_mast.aut_id) INNER JOIN category ON book_mast.cate_id = category.cate_id) INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id WHERE book_price >= ? AND book_price <= ?;';
 
 app.get('/books', (req, res) => {
 
   const category = req.query.category;
   const publisher = req.query.publisher;
+  const priceMin = req.query.priceMin;
+  const priceMax = req.query.priceMax;
+  const price = [priceMin, priceMax];
   const catAndPub = [category, publisher];
 
-  if (category && !publisher) {
+  if (category && !publisher && !priceMin) {
     conn.query(getBooksByCategory, category, (err, data) => {
       if (err) {
         console.log(err.message);
         res.status(500).send();
-        return;
       }
       else {
         res.json(data);
       }
     });
-  } else if (!category && publisher) {
+  } else if (!category && publisher && !priceMin) {
     conn.query(getBooksByPublisher, publisher, (err, data) => {
       if (err) {
         console.log(err.message);
         res.status(500).send();
-        return;
       }
       else {
         res.json(data);
       }
     });
-  } else if (publisher && category) {
+  } else if (publisher && category && !priceMin) {
     conn.query(getBooksByCategoryAndPublisher, catAndPub, (err, data) => {
       if (err) {
         console.log(err.message);
         res.status(500).send();
-        return;
+      }
+      else {
+        res.json(data);
+      }
+    });
+  } else if (priceMin && !category && !publisher) {
+    conn.query(getBooksByPrice, price, (err, data) => {
+      if (err) {
+        console.log(err.message);
+        res.status(500).send();
       }
       else {
         res.json(data);
@@ -69,7 +80,6 @@ app.get('/books', (req, res) => {
       if (err) {
         console.log(err.message);
         res.status(500).send();
-        return;
       }
       else {
         res.json(data);
