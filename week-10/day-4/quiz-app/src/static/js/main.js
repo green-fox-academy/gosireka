54016c61191e22affa3a0836f78c9b9b7242c15d
Reka Santha-Gosi <gosireka1@gmail.com>
2019-01-08 22:23:26 +0100

    feat(quiz): add eventlistener to buttons

--- week-10/day-4/quiz-app/src/static/js/main.js
+++ week-10/day-4/quiz-app/src/static/js/main.js
@@ -2,11 +2,14 @@
 
 const questionBox = document.querySelector('#box');
 
+let score = document.querySelector('#score');
+
 const displayRandomQuestion = () => {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', '/api/game');
   xhr.onload = () => {
     if (xhr.status === 200) {
+      questionBox.innerHTML = '';
       const response= JSON.parse(xhr.responseText);
       let newQuestion = document.createElement('h2');
       newQuestion.innerHTML = response.question;
@@ -14,11 +17,26 @@ const displayRandomQuestion = () => {
       response.answers.forEach(option => {
         let newButton = document.createElement('button');
         newButton.innerHTML = option.answer;
+        newButton.addEventListener('click', () => {
+          if (option.is_correct === 1){
+            newButton.setAttribute('style', 'background-color: #26AE60');
+            let scoreBefore = score.textContent;
+            score.textContent = parseInt(scoreBefore) + 1;
+          } else {
+            newButton.setAttribute('style', 'background-color: #ED8668');
+          }
+          setTimeout(()=> {
+            displayRandomQuestion();
+          }, 1500);
+          
+        });
         questionBox.appendChild(newButton);
       });
     }
   }
   xhr.send();
 }
+
+
 
 displayRandomQuestion();
